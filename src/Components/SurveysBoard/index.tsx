01f1c3f26e0da4882ddfed936afe902b77c537d9
Shya <rumarenko3@gmail.com>
2020-06-27 02:44:06 +0300

    comments

--- src/Components/SurveysBoard/index.tsx
+++ src/Components/SurveysBoard/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import axios, {AxiosResponse} from "axios"
-import { useHistory, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import authHeader from "../../Utils/authHeader";
 import {ISurvey} from "../../Interfaces/survey";
 
 
 const SurveysBoard = () => {
-  const history = useHistory();
   const [surveys, setSurveys] = React.useState<ISurvey[]>([]);
 
+  // Gets all user`s uncompleted surveys from server
   const getSurveys = React.useCallback( async () => {
     const response : AxiosResponse = await axios.get("/surveys", {headers: authHeader()})
     setSurveys(response.data.surveys);
   },[]);
+
 
   React.useEffect( () => {
     getSurveys();
@@ -20,7 +21,8 @@ const SurveysBoard = () => {
 
   return(
     <div>
-      {surveys.map( (survey,key ) => {
+      { // display all user`s uncompleted surveys
+        surveys.map( (survey,key ) => {
         return <> <Link to={`/survey/${survey._id}`} >{survey.tittle}</Link> <br/> </>
       })}
   </div>
