e06284ca5773130576137f802e0465f18675ef4e
Ilya Rymarenko <rumarenko3@gmail.com>
2020-06-28 12:46:20 +0100

    Update authHeader.ts

--- src/Utils/authHeader.ts
+++ src/Utils/authHeader.ts
@@ -1,9 +1,9 @@
 // get accessToken from local storage and wrap it as request header for auth
 export default function authHeader() {
-  const auth: string = localStorage.getItem("accessToken") || ""
+  const auth: string = localStorage.getItem("accessToken") || "" // Refresh token?
 
   if (auth) {
-    return { Authorization: `bearer ${auth}` }
+    return { Authorization: `bearer ${auth}` } // It's usually "Bearer"
   } else {
     return {}
   }
