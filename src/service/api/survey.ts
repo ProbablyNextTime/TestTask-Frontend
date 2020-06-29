5a314584b79e4d2095ed64eb373a5631d0b4094a
Shya <rumarenko3@gmail.com>
2020-06-29 14:51:17 +0300

    deploy

--- src/service/api/survey.ts
+++ src/service/api/survey.ts
@@ -3,27 +3,46 @@ import axios from "axios"
 import authHeader from "Utils/authHeader"
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || ''
 axios.defaults.withCredentials = true;
-axios.defaults.headers = authHeader();
+function setAuthToken(token: string) {
+  axios.defaults.headers.common['Authorization'] = '';
+  delete axios.defaults.headers.common['Authorization'];
 
-export const getSurveyAPI = async (surveyId: string): Promise<ISurvey> =>
-  ((await axios.get(`/surveys/?Id=${surveyId}`, { headers: authHeader() })).data.survey as unknown) as ISurvey
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = `bearer ${token}`;
+  }
+}
 
-export const postSurveyAPI = async (questions: string[], title: string): Promise<void> =>
-  await axios.post("/surveys", { questions: questions, title: title }, { headers: authHeader() })
+export const getSurveyAPI = async (surveyId: string): Promise<ISurvey> => {
+  const auth: string = localStorage.getItem("accessToken") || "";
+  setAuthToken(auth)
+  return((await axios.get(`/surveys/?Id=${surveyId}` )).data.survey as unknown) as ISurvey
+}
 
-export const postSurveyAnswerAPI = async (answers: string[], surveyId: string): Promise<void> =>
-  await axios.post(
+export const postSurveyAPI = async (questions: string[], title: string): Promise<void> => {
+  const auth: string = localStorage.getItem("accessToken") || "";
+  setAuthToken(auth)
+  await axios.post("/surveys", {questions: questions, title: title}, {headers: authHeader()})
+}
+
+export const postSurveyAnswerAPI = async (answers: string[], surveyId: string): Promise<void> => {
+    const auth: string = localStorage.getItem("accessToken") || "";
+  setAuthToken(auth)
+  return await axios.post(
     "/surveys/postAnswer/",
     {
       answers: answers,
       surveyId: surveyId,
     },
-    { headers: authHeader() }
+    {headers: authHeader()}
   )
+}
 
-export const getAvailableSurveysAPI = async (): Promise<ISurvey[]> =>
-  ((
+export const getAvailableSurveysAPI = async (): Promise<ISurvey[]> => {
+  const auth: string = localStorage.getItem("accessToken") || "";
+  setAuthToken(auth)
+  return ((
     await axios.get("/surveys", {
       headers: authHeader(),
     })
   ).data.surveys as unknown) as ISurvey[]
+}
