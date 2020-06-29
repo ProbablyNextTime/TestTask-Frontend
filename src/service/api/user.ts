b92e240ba23c673ae65e7fe432228d0ceee9167c
Shya <rumarenko3@gmail.com>
2020-06-29 03:10:57 +0300

    deploy

--- src/service/api/user.ts
+++ src/service/api/user.ts
@@ -1,14 +1,15 @@
-import axios  from "axios"
+import axios, {AxiosResponse} from "axios"
 import { IUser } from "Interfaces/user"
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || ''
 
 export const loginUserAPI = async (username: string, password: string): Promise<IUser> => {
-  const response: any = await axios.post("/login", {
+  const response: AxiosResponse = await axios.post("/login", {
     user: {
       username: username,
       password: password,
     },
   })
+  console.log(response.data);
   localStorage.setItem("accessToken", response.data.accessToken)
   return response.data.user as unknown as IUser
 }
